bd634ba3be52810f6e74a5f5b05d0a41ca2e5288
lakegreene <lakegreene@yahoo.com>
2020-09-27 01:34:37 -0400

    Delete in-memory-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -27,9 +23,6 @@ import { NavbarComponent } from './navbar/navbar.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule
-    // HttpClientInMemoryWebApiModule.forRoot(
-      // InMemoryDataService, { dataEncapsulation: false }
-    // )
   ],
   providers: [],
   bootstrap: [AppComponent]
